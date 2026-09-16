5d204f6489fd56d07115a1b14ac0d2a7e611bb8c
Matt Wilson <matthew@synesis.com.au>
2026-09-17 08:28:11 +1000

    fix

--- cmake/BuildType.cmake
+++ cmake/BuildType.cmake
@@ -5,7 +5,7 @@
 # Purpose:  CMake module file (for BuildType)
 #
 # Created:  16th October 2019
-# Updated:  12th January 2024
+# Updated:  2nd September 2026
 #
 # ######################################################################## #
 
@@ -47,7 +47,7 @@ if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
 
     message(STATUS "Setting build type to '${default_build_type}' as none was specified.")
 
-    set(CACHE CMAKE_BUILD_TYPE "${default_build_type}"
+    set(CMAKE_BUILD_TYPE "${default_build_type}" CACHE
         STRING "Choose the type of build." FORCE
     )
 
